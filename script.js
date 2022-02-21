157bea1b4b7b6c4447733ecef69bafa85a126c91
Sahil Shrestha <sahil@Sahils-MacBook-Pro.local>
2022-02-21 10:13:36 -0700

    Changed to About me

--- script.js
+++
@@ -1,9 +0,0 @@
-//getting the elements for the css magic
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navBarLinks = document.getElementsByClassName('nav-bar-links')[0];
-
-console.log("In the script, trying to get the element");
-
-toggleButton.addEventListener('click', ()=> {
-    navBarLinks.classList.toggle('active');
-});
