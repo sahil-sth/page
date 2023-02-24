dcf1f0ac6c6cff3e564b0b498f8945e70c976ff5
Sahil Shrestha <saheel.sth99@gmail.com>
2023-02-23 23:42:57 -0700

    Remove the old menu javscript handler

--- script_burger_menu.js
+++
@@ -1,9 +0,0 @@
-//getting the elements for the css magic
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navBarLinks = document.getElementsByClassName('nav-bar-links')[0];
-
-
-
-toggleButton.addEventListener('click', ()=> {
-    navBarLinks.classList.toggle('active');
-});
