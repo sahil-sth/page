f9217a138a157fe8c83ce5d9a3f1cba0378cc0a2
Sahil Shrestha <saheel.sth99@gmail.com>
2023-02-23 20:28:42 -0700

    Remove unnecessary console logs

--- script_burger_menu.js
+++ script_burger_menu.js
@@ -2,7 +2,7 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navBarLinks = document.getElementsByClassName('nav-bar-links')[0];
 
-console.log("In the script, trying to get the element");
+
 
 toggleButton.addEventListener('click', ()=> {
     navBarLinks.classList.toggle('active');
